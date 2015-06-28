b3d99454e8720be67e26bf4be5b0bc54f6b25c84
eweizman <eden_weizman@brown.edu>
2015-06-27 19:10:12 -0700

    added buttons to main activity. The text button opens up a dialog, which submits to the database whatever is inputted into it

--- Moments/app/src/main/java/moments/moments/MainActivity.java
+++ Moments/app/src/main/java/moments/moments/MainActivity.java
@@ -6,7 +6,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity implements TextInputDialog.TextSubmitter{
+
+    private ImageButton mAddText;
+    private ImageButton mAddImage;
+    private ImageButton mAddLink;
 
     private int mDataNumber = 1;
     private DataDbAdapter mDbHelper;
@@ -15,36 +19,33 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAddText = (ImageButton) findViewById(R.id.add_text);
+        mAddImage = (ImageButton) findViewById(R.id.add_image);
+        mAddLink = (ImageButton) findViewById(R.id.add_link);
         mDbHelper = new DataDbAdapter(this);
         mDbHelper.open();
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void addText(View view) {
+        new TextInputDialog().show(getFragmentManager(), "textInput");
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void submitText(String text, Type type) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+        createNote(type, text);
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    public void addImage(View view) {
 
-        return super.onOptionsItemSelected(item);
+    }
+
+    public void addLink(View view) {
+
     }
 
     private void createNote(Type type, String text) {
         String dataName = "Note " + mDataNumber++;
         mDbHelper.createNote(Type.TEXT, "test");
-        //mDbHelper.createNote(type, text);
     }
 }
