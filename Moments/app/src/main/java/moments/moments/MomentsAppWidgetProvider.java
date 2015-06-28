8643d67b312143db313e4430debd7af2506d7786
hlzhou <helenzhou1235@gmail.com>
2015-06-27 22:14:23 -0700

    widget buttons all open a text input

--- Moments/app/src/main/java/moments/moments/MomentsAppWidgetProvider.java
+++ Moments/app/src/main/java/moments/moments/MomentsAppWidgetProvider.java
@@ -5,6 +5,7 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 /**
@@ -19,17 +20,30 @@ public class MomentsAppWidgetProvider extends AppWidgetProvider {
         for (int i=0; i<N; i++) {
             int appWidgetId = appWidgetIds[i];
 
-            // Create an Intent to launch ExampleActivity
-            Intent intent = new Intent(context, WidgetActivity.class);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            // Create an Intent to launch WidgetActivity
+            Intent textIntent = new Intent(context, WTextAddActivity.class);
+            textIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            PendingIntent pendingTextIntent = PendingIntent.getActivity(context, 0, textIntent, 0);
+
+            Intent imageIntent = new Intent(context, WImageAddActivity.class);
+            imageIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            PendingIntent pendingImageIntent = PendingIntent.getActivity(context, 0, imageIntent, 0);
+
+            Intent linkIntent = new Intent(context, WLinkAddActivity.class);
+            linkIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            PendingIntent pendingLinkIntent = PendingIntent.getActivity(context, 0, linkIntent, 0);
 
             // Get the layout for the App Widget and attach an on-click listener
             // to the button
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.appwidget_provider_layout); //TODO: the layout might be something else
-            views.setOnClickPendingIntent(R.id.button, pendingIntent);
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.appwidget); //TODO: the layout might be something else
+            views.setOnClickPendingIntent(R.id.add_text, pendingTextIntent);
+            views.setOnClickPendingIntent(R.id.add_image, pendingImageIntent);
+            views.setOnClickPendingIntent(R.id.add_link, pendingLinkIntent);
 
             // Tell the AppWidgetManager to perform an update on the current app widget
             appWidgetManager.updateAppWidget(appWidgetId, views);
+
+            Log.d("HELEN", "okay logs work...");
 
         }
     }
---
+++ Moments/app/src/main/java/moments/moments/WLinkAddActivity.java
@@ -0,0 +1,21 @@
+package moments.moments;
+
+/**
+ * Created by hzhou1235 on 6/27/15.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * Created by hzhou1235 on 6/27/15.
+ */
+public class WLinkAddActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("HELEN", "YAYY IT GOT TO WIDGETACTIVITY ONCREATE");
+        setContentView(R.layout.text_input_dialog);
+    }
+}
