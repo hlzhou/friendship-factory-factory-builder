f604c8b68ed65dd8cc2cab2b6794fc386be33681
eweizman <eweizman@users.noreply.github.com>
2015-06-27 19:58:23 -0700

    Update MainActivity.java

--- Moments/app/src/main/java/moments/moments/MainActivity.java
+++ Moments/app/src/main/java/moments/moments/MainActivity.java
@@ -12,12 +12,8 @@ import android.widget.Toast;
 import fffb.moments.app.TextInputDialog;
 
 
-public class MainActivity extends Activity implements TextInputDialog.TextSubmitter{
-
-    private ImageButton mAddText;
-    private ImageButton mAddImage;
-    private ImageButton mAddLink;
-
+public class MainActivity extends Activity implements TextInputDialog.TextSubmitter, LinkInputDialog.LinkSubmitter{
+    
     private int mDataNumber = 1;
     private DataDbAdapter mDbHelper;
 
@@ -25,31 +21,36 @@ public class MainActivity extends Activity implements TextInputDialog.TextSubmit
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mAddText = (ImageButton) findViewById(R.id.add_text);
-        mAddImage = (ImageButton) findViewById(R.id.add_image);
-        mAddLink = (ImageButton) findViewById(R.id.add_link);
+        
         mDbHelper = new DataDbAdapter(this);
         mDbHelper.open();
     }
-
+    
     public void addText(View view) {
-        new TextInputDialog().show(getFragmentManager(), "textInput");
+    	TextInputDialog dialog = new TextInputDialog();
+    	dialog.show(getFragmentManager(), "textInput");
     }
-
-    public void submitText(String text, Type type) {
-        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
-        createNote(type, text);
+    
+    public void submitText(String text) {
+    	Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    	createNote(Type.TEXT, text);
     }
-
+    
+    @Override
+	public void submitLink(String text) {
+		Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    	createNote(Type.LINK, text);
+	}
+    
     public void addImage(View view) {
-
+    	
     }
-
+    
     public void addLink(View view) {
-
+    	LinkInputDialog dialog = new LinkInputDialog();
+    	dialog.show(getFragmentManager(), "linkInput");
     }
-
+    
     private void createNote(Type type, String text) {
         String dataName = "Note " + mDataNumber++;
         mDbHelper.createNote(Type.TEXT, "test");
