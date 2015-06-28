ea2a45c1d91ea5e47da8731f9a908a402c8ca752
eweizman <eweizman@users.noreply.github.com>
2015-06-28 01:34:07 -0700

    Removed camera

--- Moments/app/src/main/java/moments/moments/MomentPromptActivity.java
+++ Moments/app/src/main/java/moments/moments/MomentPromptActivity.java
@@ -29,25 +29,8 @@ public class MomentPromptActivity extends Activity implements TextInputDialog.Te
 	    	tDialog.show(getFragmentManager(), "textInput");
 	    	break;
 		case IMAGE:
-			AlertDialog.Builder builder = new AlertDialog.Builder(this);
-	    	builder.setTitle(R.string.image_source_prompt)
-	    	.setItems(R.array.image_sources, new DialogInterface.OnClickListener() {
-	            public void onClick(DialogInterface dialog, int which) {
-	            // The 'which' argument contains the index position
-	            // of the selected item
-	            switch(which){
-	            case 0:
-	            	Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-	            	startActivityForResult(takePicture, RESULT_TAKE_IMAGE);
-	            	break;
-	            case 1:
-	            	Intent getImageFromGallery = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-	                startActivityForResult(getImageFromGallery, RESULT_LOAD_IMAGE);
-	            	break;
-	            }
-	        }
-	    	});
-	    	builder.show();
+			Intent getImageFromGallery = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(getImageFromGallery, RESULT_LOAD_IMAGE);
 	    	break;
 		case LINK:
 			LinkInputDialog lDialog = new LinkInputDialog();
@@ -84,13 +67,7 @@ public class MomentPromptActivity extends Activity implements TextInputDialog.Te
         		Uri selectedImage = data.getData();
         		Toast.makeText(this, "Got an image from gallery: " + selectedImage.toString(), Toast.LENGTH_SHORT).show();
                 createNote(Type.IMAGE, selectedImage.toString());
-        	}
-        	break;
-        case RESULT_TAKE_IMAGE:
-        	if(resultCode == RESULT_OK && data != null) {
-        		Uri selectedImage = data.getData();
-        		Toast.makeText(this, "Took an image from the camera: " + selectedImage.toString(), Toast.LENGTH_SHORT).show();
-                createNote(Type.IMAGE, selectedImage.toString());
+                boolean result = MomentDisplayer.showMoment(this, new Data(Type.IMAGE, selectedImage.toString()));
         	}
         	break;
         }
